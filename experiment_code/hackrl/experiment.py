32cc6eeeb438a55b9fa9833d2a7ba1102306f2b8
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2023-04-18 08:06:43 +0200

    create optimizer as in atari DT (for big wd)

--- experiment_code/hackrl/experiment.py
+++ experiment_code/hackrl/experiment.py
@@ -23,6 +23,7 @@ import wandb
 from nle.dataset import dataset
 from nle.dataset import db
 from nle.dataset import populate_db
+from transformers.modeling_utils import Conv1D
 
 import hackrl.environment
 import hackrl.models
@@ -611,15 +612,64 @@ def compute_inverse_loss(predicted_action_logits, actions):
         predicted_action_logits.reshape(T * B, -1), actions.reshape(T * B)
     )
 
-
 def create_optimizer(model):
-    return torch.optim.AdamW(
-        model.parameters(),
-        lr=FLAGS.adam_learning_rate,
-        betas=(FLAGS.adam_beta1, FLAGS.adam_beta2),
-        eps=FLAGS.adam_eps,
-        weight_decay=FLAGS.weight_decay,
-    )
+    """
+    This long function is unfortunately doing something very simple and is being very defensive:
+    We are separating out all parameters of the model into two buckets: those that will experience
+    weight decay for regularization and those that won't (biases, and layernorm/embedding weights).
+    We are then returning the PyTorch optimizer object.
+    """
+
+    # separate out all parameters to those that will and won't experience regularizing weight decay
+    decay = set()
+    no_decay = set()
+    # whitelist_weight_modules = (torch.nn.Linear, )
+    whitelist_weight_modules = (torch.nn.Linear, torch.nn.Conv2d, Conv1D)
+    blacklist_weight_modules = (torch.nn.LayerNorm, torch.nn.Embedding)
+    modules = set()
+    for mn, m in model.named_modules():
+        for pn, p in m.named_parameters():
+            fpn = '%s.%s' % (mn, pn) if mn else pn # full param name
+
+            if pn.endswith('bias'):
+                # all biases will not be decayed
+                no_decay.add(fpn)
+            elif pn.endswith('weight') and isinstance(m, whitelist_weight_modules):
+                # weights of whitelist modules will be weight decayed
+                decay.add(fpn)
+            elif pn.endswith('weight') and isinstance(m, blacklist_weight_modules):
+                # weights of blacklist modules will NOT be weight decayed
+                no_decay.add(fpn)
+            elif m._get_name() == "RecursiveScriptModule" and pn == "weight":
+                decay.add(fpn)
+
+    # validate that we considered every parameter
+    param_dict = {pn: p for pn, p in model.named_parameters()}
+    inter_params = decay & no_decay
+    union_params = decay | no_decay
+    assert len(inter_params) == 0, "parameters %s made it into both decay/no_decay sets!" % (str(inter_params), )
+    assert len(param_dict.keys() - union_params) == 0, "parameters %s were not separated into either decay/no_decay set!" \
+                                                % (str(param_dict.keys() - union_params), )
+
+    # create the pytorch optimizer object
+    optim_groups = [
+        {"params": [param_dict[pn] for pn in sorted(list(decay))], "weight_decay": FLAGS.weight_decay},
+        {"params": [param_dict[pn] for pn in sorted(list(no_decay))], "weight_decay": 0.0},
+    ]
+    optimizer = torch.optim.AdamW(optim_groups, lr=FLAGS.adam_learning_rate, betas=(FLAGS.adam_beta1, FLAGS.adam_beta2))
+    return optimizer
+
+
+# def create_optimizer(model):
+#     return torch.optim.AdamW(
+#         model.parameters(),
+#         lr=FLAGS.adam_learning_rate,
+#         betas=(FLAGS.adam_beta1, FLAGS.adam_beta2),
+#         eps=FLAGS.adam_eps,
+#         weight_decay=FLAGS.weight_decay,
+#     )
+
+
 
 
 def create_scheduler(optimizer):
