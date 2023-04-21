93645857e1e4a8aab1812297b7c5a039c0c7fe6a
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2023-04-21 16:00:05 +0200

    eval more parameters and argparse

--- experiment_code/eval.py
+++ experiment_code/eval.py
@@ -1,5 +1,7 @@
 import os
-import sys
+import argparse
+
+from pathlib import Path
 
 import moolib
 import numpy as np
@@ -33,29 +35,36 @@ def load_model_and_flags(path, device):
     return model, flags
 
 
-def generate_envpool_rollouts(model, rollouts, flags, pbar_idx=0, score_target=10000):
+def generate_envpool_rollouts(
+    model, 
+    flags, 
+    rollouts = 1024, 
+    batch_size=512,
+    num_actor_cpus = 20,
+    num_actor_batches = 2,
+    pbar_idx=0, 
+    score_target=10000
+):
     global ENVS
     # NB: We do NOT want to generate the first N rollouts from B batch
     # of envs since this will bias short episodes.
     # Instead lets just allocate some episodes to each env
-    num_batches = 2
-    split = 4
-    rollouts = rollouts // (num_batches * split)
-    flags.batch_size = rollouts
+    split = rollouts // (batch_size * num_actor_batches)
+    flags.batch_size = batch_size
     device = flags.device
 
     ENVS = moolib.EnvPool(
         lambda: hackrl.environment.create_env(flags),
-        num_processes=40,
-        batch_size=rollouts,
-        num_batches=num_batches,
+        num_processes=num_actor_cpus,
+        batch_size=batch_size,
+        num_batches=num_actor_batches,
     )
 
     rollouts_left = (
         torch.ones(
             (
-                num_batches,
-                rollouts,
+                num_actor_batches,
+                batch_size,
             )
         )
         .long()
@@ -64,14 +73,14 @@ def generate_envpool_rollouts(model, rollouts, flags, pbar_idx=0, score_target=1
     )
     current_reward = torch.zeros(
         (
-            num_batches,
-            rollouts,
+            num_actor_batches,
+            batch_size,
         )
     ).to(device)
     timesteps = torch.zeros(
         (
-            num_batches,
-            rollouts,
+            num_actor_batches,
+            batch_size,
         )
     ).to(device)
 
@@ -79,18 +88,18 @@ def generate_envpool_rollouts(model, rollouts, flags, pbar_idx=0, score_target=1
     results = [None, None]
     grand_pbar = tqdm.tqdm(position=0, leave=True)
     pbar = tqdm.tqdm(
-        total=rollouts * num_batches * split, position=pbar_idx + 1, leave=True
+        total=batch_size * num_actor_batches * split, position=pbar_idx + 1, leave=True
     )
 
-    action = torch.zeros((num_batches, rollouts)).long().to(device)
-    hs = [model.initial_state(rollouts) for _ in range(num_batches)]
+    action = torch.zeros((num_actor_batches, batch_size)).long().to(device)
+    hs = [model.initial_state(batch_size) for _ in range(num_actor_batches)]
     hs = nest.map(lambda x: x.to(device), hs)
 
     totals = torch.sum(rollouts_left).item()
-    subtotals = [torch.sum(rollouts_left[i]).item() for i in range(num_batches)]
+    subtotals = [torch.sum(rollouts_left[i]).item() for i in range(num_actor_batches)]
     while totals > 0:
         grand_pbar.update(1)
-        for i in range(num_batches):
+        for i in range(num_actor_batches):
             if subtotals[i] == 0:
                 continue
             if results[i] is None:
@@ -130,32 +139,32 @@ def generate_envpool_rollouts(model, rollouts, flags, pbar_idx=0, score_target=1
 
 
 def find_checkpoint(path, min_steps, device):
-    versions = []
-    flags = omegaconf.OmegaConf.create(
-        torch.load(path + "/checkpoint.tar", map_location=torch.device(device))["flags"]
-    )
-    for f in os.listdir(path):
-        ff = str(f)
-        if ff.startswith("checkpoint_v"):
-            v = int(ff.replace("checkpoint_v", "").replace(".tar", ""))
-            versions.append(v)
-    desired_v = min_steps / (flags.batch_size * flags.unroll_length)
-    for v in sorted(versions):
-        allowed_paths = [
-            "/checkpoint/ehambro/20220531/meek-binturong",
-            "/checkpoint/ehambro/20220531/adamant-viper",
-            "/checkpoint/ehambro/20220531/hallowed-bat",
-            "/checkpoint/ehambro/20220531/celadon-llama",
-        ]
-        if v > desired_v or v > 122070.325 or (path in allowed_paths and v > 18000):
-            return f"{path}/checkpoint_v{v}.tar"
+    # versions = []
+    # flags = omegaconf.OmegaConf.create(
+    #     torch.load(path / "checkpoint.tar", map_location=torch.device(device))["flags"]
+    # )
+    # for f in os.listdir(path):
+    #     ff = str(f)
+    #     if ff.startswith("checkpoint_v"):
+    #         v = int(ff.replace("checkpoint_v", "").replace(".tar", ""))
+    #         versions.append(v)
+    # desired_v = min_steps / (flags.batch_size * flags.unroll_length)
+    # for v in sorted(versions):
+    #     allowed_paths = [
+    #         "/checkpoint/ehambro/20220531/meek-binturong",
+    #         "/checkpoint/ehambro/20220531/adamant-viper",
+    #         "/checkpoint/ehambro/20220531/hallowed-bat",
+    #         "/checkpoint/ehambro/20220531/celadon-llama",
+    #     ]
+    #     if v > desired_v or v > 122070.325 or (path in allowed_paths and v > 18000):
+    #         return f"{path}/checkpoint_v{v}.tar"
     # return f"{path}/checkpoint_v{v}.tar"
 
     print("Returning checkpoint.tar")
     return f"{path}/checkpoint.tar"
 
 
-def evaluate_folder(name, path, min_steps, device, rollouts, pbar_idx, score_target):
+def evaluate_folder(name, path, min_steps, device, pbar_idx, output_dir, **kwargs):
     p_ckpt = find_checkpoint(path, min_steps, device)
     if not p_ckpt:
         print(f"Not yet: {name} - {path}")
@@ -168,31 +177,69 @@ def evaluate_folder(name, path, min_steps, device, rollouts, pbar_idx, score_tar
             -1,
         )
     print(f"{pbar_idx} {name} Using: {p_ckpt}")
-    os.makedirs(f"{DIR}/{NAME}/", exist_ok=True)
+    save_dir = output_dir / name
+    save_dir.mkdir(parents=True, exist_ok=True)
     model, flags = load_model_and_flags(p_ckpt, device)
-    returns = generate_envpool_rollouts(model, rollouts, flags, pbar_idx, score_target)
+    returns = generate_envpool_rollouts(
+        model=model, 
+        flags=flags, 
+        pbar_idx=pbar_idx, 
+        **kwargs,
+    )
     return (name, p_ckpt) + returns
 
 
-if __name__ == "__main__":
-    DIR = "results_txt"
-    NAME, PATH, SCORE_TARGET = sys.argv[1], sys.argv[2], sys.argv[3]
-    DEVICE = sys.argv[3] if len(sys.argv) == 4 else "cuda:0"
-    print(f"Running {NAME} - {PATH} on {DEVICE}")
-    MIN_STEPS = 1_000_000_000
-    ROLLOUTS = 1024
+def parse_args(args=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--name", type=str)
+    parser.add_argument("--checkpoint_dir", type=Path)
+    parser.add_argument("--output_dir", type=Path)
+    parser.add_argument("--min_steps", type=int, default=1_000_000_000)
+    parser.add_argument("--rollouts", type=int, default=1024)
+    parser.add_argument("--batch_size", type=int, default=512)
+    parser.add_argument("--num_actor_cpus", type=int, default=20)
+    parser.add_argument("--num_actor_batches", type=int, default=2)
+    parser.add_argument("--device", type=str, default="cuda")
+    parser.add_argument("--score_target", type=float)
+    return parser.parse_known_args(args=args)[0]
 
-    results = (NAME, PATH, -1, -1, -1)
-    results = evaluate_folder(NAME, PATH, MIN_STEPS, DEVICE, ROLLOUTS, 0, SCORE_TARGET)
+
+def main(
+    name: str, 
+    checkpoint_dir: Path, 
+    output_dir: Path, 
+    rollouts: int,
+    min_steps: int, 
+    device: str, 
+    **kwargs
+):
+    results = (name, checkpoint_dir, -1, -1, -1)
+
+    results = evaluate_folder( 
+        name=name, 
+        path=checkpoint_dir, 
+        min_steps=min_steps, 
+        device=device, 
+        pbar_idx=0, 
+        output_dir=output_dir,
+        rollouts=rollouts,
+        **kwargs
+    )
+
     print(
         f"{results[0]} Done {results[1]}  Mean {results[3]} ± {results[4]}  | Median {results[5]}"
     )
     if results[2] > -2:
         data = (
-            MIN_STEPS,
-            ROLLOUTS,
+            min_steps,
+            rollouts,
         ) + results
-        os.makedirs(f"{DIR}/{NAME}/", exist_ok=True)
-        with open(f"{DIR}/{NAME}/{PATH.split('/')[-1]}.txt", "w") as f:
+        os.makedirs(f"{output_dir}/{name}/", exist_ok=True)
+        with open(f"{output_dir}/{name}/{checkpoint_dir.split('/')[-1]}.txt", "w") as f:
             f.write(",".join(str(d) for d in data) + "\n")
     print("done")
+
+
+if __name__ == "__main__":
+    args = vars(parse_args())
+    main(**args)
